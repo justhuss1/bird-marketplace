51d4bda27300c0059c0d6704b748455d7825db14
Hussein Samman <justhuss1@gmail.com>
2026-04-08 19:19:56 +1000

    UI update - categories section

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -64,7 +64,7 @@ export default function CreateListingPage() {
         formData.append("upload_preset", uploadPreset);
 
         const res = await fetch(
-          `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+          `https://api.cloudinary.com/v1_1/$ddqd3aauy/image/upload`,
           {
             method: "POST",
             body: formData,
